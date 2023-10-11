23cc467d8373175af912f33d27ef45de19f9fd4b
Anngrybird1201 <145516036+Anngrybird1201@users.noreply.github.com>
2023-10-11 02:25:26 -0700

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+
+MONGO_URI = `mongodb://127.0.0.1:27017/Chit-Chat-master`;
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.bold);
+    process.exit();
+  }
+};
+
+module.exports = connectDB;
